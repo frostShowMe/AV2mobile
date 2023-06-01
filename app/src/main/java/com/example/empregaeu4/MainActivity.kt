f7fc91479e7dfe09cbaafe2ac9e35caaccaafc04
segunda avaliação <lucianofabio921@gmail.com>
2023-05-31 23:40:29 -0300

    segunda avaliação

--- app/src/main/java/com/example/empregaeu4/MainActivity.kt
+++ app/src/main/java/com/example/empregaeu4/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.empregaeu4.databinding.ActivityMainBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -19,11 +21,14 @@ class MainActivity : AppCompatActivity() {
 
 
         btLogin.setOnClickListener {
-                if(idNome.text.toString() == "Marcos" && idSenha.text.toString().toIntOrNull() == 384) {
+                if(idNome.text.toString() == "fabio" && idSenha.text.toString().toIntOrNull() == 384) {
                     val intent = Intent(this, Tela2Activity::class.java)
                     val texto = idNome.text.toString()
                     intent.putExtra("nome", texto)
                     startActivity(intent)
+                    val database= Firebase.database
+                    val myRef = database.getReference("message")
+                    myRef.setValue("Hello World")
                 } else{
                     Toast.makeText(this, R.string.msgError, Toast.LENGTH_SHORT).show()
                 }
